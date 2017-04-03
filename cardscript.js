74d86d18b6e6ebf0e8629653e946dc89ba3f82b1
Henry Gruett <henrikgruett@gmail.com>
2017-04-03 17:14:14 -0500

    Added CSS link

--- cardscript.js
+++ cardscript.js
@@ -1,3 +1,15 @@
+var cssId = 'CSSelement';
+if (!document.getElementById(CSSelement))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = './stylesheet.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
 
 //Hides and Shows
 hideElement(){
