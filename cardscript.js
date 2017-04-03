dc191a2a4141a71e867c51f7f1d0e4e8fb064fac
Henry Gruett <henrikgruett@gmail.com>
2017-04-03 17:12:46 -0500

    Added show and hide

--- cardscript.js
+++ cardscript.js
@@ -1,1 +1,10 @@
 
+//Hides and Shows
+hideElement(){
+  document.getElementById('id').classList.add('hideElement');
+  document.getElementById('id').classList.remove('showElement');
+}
+showElement(){
+  document.getElementById('id').classList.add('showElement');
+  document.getElementById('id').classList.remove('hideElement');
+}
